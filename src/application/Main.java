c3bcdd5b3750e4238d2999eec059e544839bbeb3
pablo-cruz2022 <73622143+pablo-cruz2022@users.noreply.github.com>
2020-12-03 17:45:09 -0600

    Added Java Doc to Main
    Java Doc comments have been updated for the Main.java file.

--- src/application/Main.java
+++ src/application/Main.java
@@ -9,9 +9,11 @@ import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
 /**
- * Main class, sets stage for scene viewer
- * @author lvz774
- *
+ * Part of the application package: Initiates the launch of the application and sets the stage and main
+ * scene.
+ * 
+ * @author  lvz774
+ * @version 1.0
  */
 public class Main extends Application {
 	
@@ -26,10 +28,10 @@ public class Main extends Application {
 	}
 
 	/**
-	 * loads view
-	 * @throws IOException
+	 * Loads and shows the main view of the application from the file {@code Main.fxml}.
+	 * 
+	 * @throws IOException if an error occure while loading the FXML document.
 	 */
-	
 	private void showMainView() throws IOException{
 		FXMLLoader loader = new FXMLLoader();
 		loader.setLocation(Main.class.getResource("controller/Main.fxml"));
@@ -41,8 +43,9 @@ public class Main extends Application {
 	}
 	
 	/**
-	 * launch
-	 * @param args
+	 * Launches the application.
+	 * 
+	 * @param args command line arguments, will not be used.
 	 */
 	public static void main(String[] args) {
 		launch(args);
